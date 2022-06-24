e1b67aeeae1331023b4014ddc2725b608e22f012
passos <pinheiropassosm@gmail.com>
2022-06-24 20:02:16 -0300

    add some validators to models

--- receitapp/validators/model_validator.py
+++ receitapp/validators/model_validator.py
@@ -1,6 +1,12 @@
-class ReceitaFormatoTempo(Exception):
-    ''' 
-        - Classe de excecao que deve ser subida caso tempo de preparo seja diferente do format hh:mm
-        - Fazer validacao utlizando regex
-    '''
-    pass
+import re
+
+
+def RegexValidatorTempoPreparo(value):
+    regex_validator = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+    if not bool(re.match(regex_validator, value)):
+        raise Exception('Formato de tempo inválido')
+
+
+def verifica_se_imagem_valida(file):
+    if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
+        raise Exception('Formato de imagem inválido')
